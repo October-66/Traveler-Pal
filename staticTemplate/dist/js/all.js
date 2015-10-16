c69d3a8d737cddf213d32390dbd382e2c5335eb0
rccoder <rccoder.net@gmail.com>
2015-10-16 21:33:07 +0800

    Done static index

--- staticTemplate/dist/js/all.js
+++ staticTemplate/dist/js/all.js
@@ -1,16 +1,35 @@
 $(function($) {
-    $('#swiper-item').swiper({
-    pagination: '#swiper-item .swiper-pagination',
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflow: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true
-    }
-  });
+    $('#swiper-show').swiper({
+        pagination: '#swiper-show .swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: false,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true
+        }
+    });
+
+    $('#swiper-activity').swiper({
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        spaceBetween: 30,
+        grabCursor: true
+    });
+
+    $('#swiper-scenery').swiper({
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        spaceBetween: 30,
+        grabCursor: true
+    });
+
 });
