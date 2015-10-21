46a84c21705da0328301d023e4e6e0f87e52572e
rccoder <rccoder.net@gmail.com>
2015-10-21 10:41:22 +0800

    set gulp

---
+++ traveler_pal/app/static/gulpfile.js
@@ -0,0 +1,54 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
+
+// 检查脚本
+gulp.task('lint', function() {
+    return gulp.src('./tep/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// 编译Sass
+gulp.task('sass', function() {
+    return gulp.src('./tep/scss/app.scss')
+        .pipe(sass())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./css'));
+});
+
+// 合并，压缩文件
+gulp.task('scripts', function() {
+    return gulp.src('./tep/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'));
+});
+
+gulp.task('clean', function(callback) {
+    del(['./dist/css/app.min.css', './js/all.min.js'], callback)
+});
+
+// 默认任务
+gulp.task('default', function(){
+    gulp.run('lint', 'sass', 'scripts', 'clean');
+
+    // 监听文件变化
+    gulp.watch('./tep/js/*.js', function(){
+        gulp.run('lint', 'sass', 'scripts', 'clean');
+    });
+
+    gulp.watch('./tep/scss/*.scss', function(){
+        gulp.run('lint', 'sass', 'scripts', 'clean');
+    });
+});
---
+++ traveler_pal/app/static/tep/js/app.js
@@ -0,0 +1,35 @@
+$(function($) {
+    $('#swiper-show').swiper({
+        pagination: '#swiper-show .swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: false,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true
+        }
+    });
+
+    $('#swiper-activity').swiper({
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        spaceBetween: 30,
+        grabCursor: true
+    });
+
+    $('#swiper-scenery').swiper({
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        spaceBetween: 30,
+        grabCursor: true
+    });
+
+});
