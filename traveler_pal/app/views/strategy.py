0656dc632693b173986ac7ea590a516b6cc6f37c
徐茁 <861265654@qq.com>
2015-12-16 08:01:21 +0800

    刚刚revert，一直推不上去，就强推一波了。--按照景点的写法写了getallstrategy

---
+++ traveler_pal/app/views/strategy.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+from django.contrib.auth.decorators import *
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from ..models import *
+
+
+def getAllStrategy(request):
+    """
+    所有攻略
+    """
+    limit = 5
+    allStrategy = Strategy.objects.all()
+    pagtor = Paginator(allStrategy, 5)
+    page = request.GET.get('page')
+    try:
+        pagedStgy = pagtor.page(page)
+    except EmptyPage:
+        pagedStgy = pagtor.page(pagtor.num_pages)
+    except InvalidPage:
+        pagedStgy = pagtor.page(1)
+    return render_to_response("strategy.html", RequestContext(request, {
+        "active": "strategy",
+        "allStrategy": pagedStgy
+    }))
+
+
+@login_required
+def addStrategy(request):
+    newStrategy = Strategy.objects.create(
+
+    )
